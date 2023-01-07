05a2c679046bf66ed5f52a45763aeb9a30b91d71
SimpliAj <122173112+SimpliAj@users.noreply.github.com>
2023-01-07 19:11:35 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,14 +5,25 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 -- Register a network event to receive the item and job data from the client
 RegisterNetEvent("playerHasItem")
 
+-- Create a table to store the valid job and item combinations
+local jobItemCombinations = {
+    ["job_name_1"] = "item_name_1",
+    ["job_name_2"] = "item_name_2",
+    ["job_name_3"] = "item_name_3"
+}
+
 -- Create a function to handle the network event
 function playerHasItem(item, job)
     -- Check if the player has the specified item and job
     if HasItem(item) and GetPlayerJob() == job then
-        -- Add a blip for the player on the map
-        AddBlipForPlayer(player)
+        -- Check if the item and job combination is valid
+        if jobItemCombinations[job] == item then
+            -- Add a blip for the player on the map
+            AddBlipForPlayer(player)
+        end
     end
 end
 
 -- Add an event handler for the network event
 AddEventHandler("playerHasItem", playerHasItem)
+
